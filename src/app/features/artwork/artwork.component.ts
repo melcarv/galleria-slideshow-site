4710ccf0cb11b3bec3597176fd3d47f462bcccc8
melcarv <melm.dev@gmail.com>
2025-03-06 22:07:35 -0300

    Criação da arquitetura do projeto

---
+++ src/app/features/artwork/artwork.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-artwork',
+  imports: [],
+  templateUrl: './artwork.component.html',
+  styleUrl: './artwork.component.scss'
+})
+export class ArtworkComponent {
+
+}
---
+++ src/app/features/shared/image-modal/image-modal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-image-modal',
+  imports: [],
+  templateUrl: './image-modal.component.html',
+  styleUrl: './image-modal.component.scss'
+})
+export class ImageModalComponent {
+
+}
